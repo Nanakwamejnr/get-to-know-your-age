ec6772e5f82bd658c37ac3e3dbe039ab6406625a
Nanakwamejnr <jamponsah550@gmail.com>
2022-02-14 02:52:00 +0000

    first commit

---
+++ get_to_know_age.py
@@ -0,0 +1,7 @@
+print("welcome to know your age")
+name = input("what is your name " )
+age = int(input("what is your age"))
+if age>=18:
+    print("you can drive a car")
+else:
+    print("you can't drive a car")
